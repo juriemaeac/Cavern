fc06928f14114e425591db730c45acd59ddb4b0c
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2021-02-18 21:11:21 +0800

    draft

--- Cavern/LOVE2D/source/startup/loadSounds.lua
+++ Cavern/LOVE2D/source/startup/loadSounds.lua
@@ -6,7 +6,7 @@ sounds.splash = love.audio.newSource("sounds/player/splash.wav", "static")
 
 -- There are multiple laser sound effects, was originally planning to have
 -- it randomly play one, but I decided I like laser2.wav the best.
-sounds.laser = love.audio.newSource("sounds/player/laser2.wav", "static")
+sounds.laser = love.audio.newSource("sounds/player/arrow.mp3", "static")
 sounds.spear = love.audio.newSource("sounds/player/spear.wav", "static")
 --sounds.laser = love.audio.newSource({"sounds/player/laser1.wav", "sounds/player/laser2.wav", "sounds/player/laser3.wav"}, "static")
 sounds.bombShot = love.audio.newSource("sounds/player/bombShot.wav", "static")
